8b51b9c9821e746997ab51dad8d3ff7796232f02
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-11-04 19:45:40 +0100

    build: add moment.js

--- src/app/components/list-repos/list-repos.component.ts
+++ src/app/components/list-repos/list-repos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Repos } from '../../models/Repos';
 import { ReposService }  from '../../services/repos.service'
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-list-repos',
