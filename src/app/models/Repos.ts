ff3536b7b8affd14b4a077ddfa3cc86560259ce3
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-11-04 14:04:32 +0100

    feat: add Repos's model
    using objects/classes which hold our data may be pretty useful
    it makes the developpement easier

---
+++ src/app/models/Repos.ts
@@ -0,0 +1,10 @@
+export class Repos{
+    id:number;
+    Repository_name:string;
+    Repository_description:string;
+    Owner_avatar:string;
+    Nbr_stars:number;
+    Nbr_issues:number;
+    Time_interval:number;
+    Owner_name:string
+}
