77b4660b5573404daaba989fb12be19884dfc311
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-11-04 11:35:28 +0100

    feat: create HTML CSS Mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListReposComponent } from './components/list-repos/list-repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListReposComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/list-repos/list-repos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-repos',
+  templateUrl: './list-repos.component.html',
+  styleUrls: ['./list-repos.component.css']
+})
+export class ListReposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
