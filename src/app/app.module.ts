e529b8e68932e01223064ae8abefe8f1664d5473
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-11-06 14:44:04 +0100

    feat: add Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ListReposComponent } from './components/list-repos/list-repos.component';
 import { ItemReposComponent } from './components/item-repos/item-repos.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListReposComponent,
     ItemReposComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
